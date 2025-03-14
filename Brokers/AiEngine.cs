745fd93d34fc7e60d4fb9afc2c4d65ab1ba8ae52
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2025-03-14 19:11:23 +0200

    Update AiEngine.cs

--- Brokers/AiEngine.cs
+++ Brokers/AiEngine.cs
@@ -3,14 +3,14 @@ namespace Blazor.AI.Chat.Brokers;
 public class AiEngine(IChatClient ChatClient)
 {
     public List<ChatMessage> Conversation { get; private set; } = [new(ChatRole.System, "You are a helpful AI assistant, Do not talk a lot, keep it short and simple")];
-    public IAsyncEnumerable<StreamingChatCompletionUpdate> StreamResponseAsync(string message)
+    public IAsyncEnumerable<ChatResponseUpdate> StreamResponseAsync(string message)
     {
         Conversation.Add(new(ChatRole.User, message));
-        return ChatClient.CompleteStreamingAsync(Conversation); 
+        return ChatClient.GetStreamingResponseAsync(Conversation);
     }
     public async ValueTask<string?> GetResponseAsync(string message)
     {
         Conversation.Add(new(ChatRole.User, message));
-        return (await ChatClient.CompleteAsync(Conversation)).Message.Text?.Trim();
+        return (await ChatClient.GetResponseAsync(Conversation)).Text?.Trim();
     }
 }
